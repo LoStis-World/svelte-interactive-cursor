ee5708849eeab8528c25279857ddc8be1be8d092
LoStisWorld <david.in.bergen@gmail.com>
2025-01-11 15:23:38 +0100

    Export additional types and functions from interactiveCursor module for improved usability

--- src/lib/interactive-cursor/index.ts
+++ src/lib/interactive-cursor/index.ts
@@ -1,3 +1,17 @@
 import InteractiveCursor from './interactive-cursor.svelte';
+import {
+	type ScaleOnActiveElement,
+	type InteractiveCursorOptions,
+	type InitialCursor,
+	type ActiveDataValue,
+	interactiveCursor as interactiveCursorFN
+} from './interactiveCursor.svelte';
 
 export default InteractiveCursor;
+export {
+	interactiveCursorFN,
+	type InteractiveCursorOptions,
+	type ScaleOnActiveElement,
+	type InitialCursor,
+	type ActiveDataValue
+};
